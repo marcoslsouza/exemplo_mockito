fd52f7aaad6efcb4c2c897c14033362e3abd5182
marcoslsouza <marcoslsouzadf@gmail.com>
2021-10-06 18:49:34 -0300

    Manipulando o comportamento do mock

--- src/test/java/br/com/marcoslsouza/leilao/service/FinalizarLeilaoServiceTest.java
+++ src/test/java/br/com/marcoslsouza/leilao/service/FinalizarLeilaoServiceTest.java
@@ -4,9 +4,11 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import br.com.marcoslsouza.leilao.dao.LeilaoDao;
@@ -34,7 +36,25 @@ class FinalizarLeilaoServiceTest {
 		
 		List<Leilao> leiloes = this.leiloes();
 		
+		// Mockito.when => Quando esse metodo for chamado, devolva a lista leiloes. 
+		// (Para nao retornar a lista fazia)
+		Mockito.when(dao.buscarLeiloesExpirados()).thenReturn(leiloes);
+		
 		this.service.finalizarLeiloesExpirados();
+		
+		// Fazer os assert.
+		
+		// Só tem um leilao
+		Leilao leilao = leiloes.get(0);
+		
+		// Verifica se o leilao finalizou (Fechou)
+		Assert.assertTrue(leilao.isFechado());
+		
+		// Verifica se o lance vencedor e o de 900
+		Assert.assertEquals(new BigDecimal("900"), leilao.getLanceVencedor().getValor());
+		
+		// Verificar se os dados no metodo no service foram salvos
+		Mockito.verify(dao).salvar(leilao);
 	}
 
 	// Metodo para criar uma lista de leiloes, pois o Mock retornara uma lista vazia em FinalizarLeilaoService->finalizarLeiloesExpirados 
@@ -43,7 +63,7 @@ class FinalizarLeilaoServiceTest {
 		List<Leilao> lista = new ArrayList();
 		Leilao leilao = new Leilao("Celular", new BigDecimal("500"), new Usuario("Fulano"));
 		
-		Lance primeiro = new Lance(new Usuario("Beltrano"), new BigDecimal("600"));
+		Lance primeiro = new Lance(new Usuario("Beltrano"), new BigDecimal("900"));
 		Lance segundo = new Lance(new Usuario("Beltrano"), new BigDecimal("600"));
 		
 		leilao.propoe(primeiro);
